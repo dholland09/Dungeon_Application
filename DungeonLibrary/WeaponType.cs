913eb395f6082a17802309d6beb46d9f65d63c68
Derek Holland <dholland@live.com>
2022-04-06 16:22:53 -0500

    Finished block3labs

---
+++ DungeonLibrary/WeaponType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public enum WeaponType
+    {
+        RocketLauncer = 1,
+        Sword = 2,
+        Knife = 3,
+        Project = 4,
+        Laser = 5
+
+    }
+}
